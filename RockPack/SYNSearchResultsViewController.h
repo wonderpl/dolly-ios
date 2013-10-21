0bf1138e5643eaa3007fbe05b5191c7878592dd7
Michael Michailidis <mmick66@gmail.com>
2013-10-21 14:44:31 +0100

    Added search result classes

---
+++ RockPack/SYNSearchResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYNSearchResultsViewController.h
+//  dolly
+//
+//  Created by Michael Michailidis on 21/10/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import "SYNAbstractViewController.h"
+
+@interface SYNSearchResultsViewController : SYNAbstractViewController
+
+@end
---
+++ RockPack/SYNSearchResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SYNSearchResultsViewController.m
+//  dolly
+//
+//  Created by Michael Michailidis on 21/10/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import "SYNSearchResultsViewController.h"
+
+@interface SYNSearchResultsViewController ()
+
+@end
+
+@implementation SYNSearchResultsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
