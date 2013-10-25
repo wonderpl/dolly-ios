2ad5f047a4d25646307623e43b393a6f9fddad27
CongLeRockPack <Unbounded@Unboundeds-MacBook-Pro-2.local>
2013-10-25 11:57:53 +0100

    ProfileRootView nib changes.

--- RockPack/SYNProfileRootViewController.h
+++ RockPack/SYNProfileRootViewController.h
@@ -9,18 +9,10 @@
 #import "SYNAbstractViewController.h"
 #import "ChannelOwner.h"
 
-@protocol MoveTabDelegate <NSObject>
-@optional
--(void)moveTab:(UIScrollView*)scrollView;
-@end
-
-
 @interface SYNProfileRootViewController : SYNAbstractViewController
 
 @property (nonatomic, strong) ChannelOwner* channelOwner;
 @property (nonatomic, assign) BOOL hideUserProfile;
-@property (nonatomic,assign) id<MoveTabDelegate> moveTabDelegate;
 
-- (void) deleteChannel;
 
 @end
