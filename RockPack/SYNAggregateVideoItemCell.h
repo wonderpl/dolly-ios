f54d36f46a14459440e7fb6ade87c2ae350b6445
Nick Banks <nick.banks@synchromation.com>
2013-10-30 16:31:53 +0000

    minor reformatting

--- RockPack/SYNAggregateVideoItemCell.h
+++ RockPack/SYNAggregateVideoItemCell.h
@@ -6,9 +6,9 @@
 //  Copyright (c) 2013 Nick Banks. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "VideoInstance.h"
 #import "SYNSocialActionsDelegate.h"
+#import "VideoInstance.h"
+#import <UIKit/UIKit.h>
 
 @interface SYNAggregateVideoItemCell : UICollectionViewCell
 
@@ -16,9 +16,8 @@
 @property (nonatomic, strong) IBOutlet UILabel* timeLabel;
 @property (nonatomic, strong) IBOutlet UILabel* titleLabel;
 
-
-
 // data related
 @property (nonatomic, weak) id<SYNSocialActionsDelegate> delegate;
 @property (nonatomic, weak) VideoInstance* videoInstance;
+
 @end
