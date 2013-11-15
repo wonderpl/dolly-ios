d13776857d087771a74b611ee817b2b48b73d652
Nick Banks <nick.banks@synchromation.com>
2013-11-15 11:34:39 +0000

    Fixed text fields on iPhone signup/login

---
+++ RockPack/SYNTextFieldLoginiPhone.h
@@ -0,0 +1,13 @@
+//
+//  SYNSYNTextFieldLoginiPhone.h
+//  dolly
+//
+//  Created by Nick Banks on 15/11/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SYNTextFieldLoginiPhone : UITextField
+
+@end
