f4d937236c9318063f1a3a1586223cfe6716fd37
Nick Banks <nick.banks@synchromation.com>
2013-11-15 11:23:40 +0000

    Fixed bizarre text offset problem in UITextFields in signup page

---
+++ RockPack/SYNTextFieldLogin.h
@@ -0,0 +1,13 @@
+//
+//  SYNTextFieldLogin.h
+//  rockpack
+//
+//  Created by Nick Banks on 15/11/2013.
+//  Copyright (c) Rockpack Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNTextFieldLogin : UITextField
+
+@end
