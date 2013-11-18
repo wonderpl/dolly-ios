956b73f7267e38acb2aa894eba7a130abe830a96
Michael Michailidis <michaelmichailidis@Guys-MacBook-Pro.local>
2013-11-18 13:51:30 +0000

    Adding styling to the text view of the About section

--- RockPack/SYNAboutViewController.m
+++ RockPack/SYNAboutViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "SYNAboutViewController.h"
+#import "UIColor+SYNColor.h"
+#import "UIFont+SYNFont.h"
 
 @interface SYNAboutViewController ()
 
@@ -23,7 +25,42 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     self.title = @"Information";
+    
+    
+    // Style the content of the TextView
+    
+    NSMutableAttributedString* attributedMutString = [[NSMutableAttributedString alloc] init];
+    
+    NSMutableParagraphStyle *paragrapStyleCenter = [[NSMutableParagraphStyle alloc] init];
+    paragrapStyleCenter.alignment = NSTextAlignmentCenter;
+    
+    NSMutableParagraphStyle *paragrapStyleLeft = [[NSMutableParagraphStyle alloc] init];
+    paragrapStyleLeft.alignment = NSTextAlignmentLeft;
+    
+    
+    [attributedMutString appendAttributedString: [[NSAttributedString alloc] initWithString: @"Mayberry v1.0.4\n"
+                                                                                 attributes: @{NSForegroundColorAttributeName: [UIColor blackColor],
+                                                                                               NSParagraphStyleAttributeName: paragrapStyleCenter,
+                                                                                               NSFontAttributeName: [UIFont regularCustomFontOfSize:23]}]];
+    
+    [attributedMutString appendAttributedString: [[NSAttributedString alloc] initWithString: @"@ 2013 Rockpack Ltd.\n\n\n"
+                                                                                 attributes: @{NSForegroundColorAttributeName: [UIColor dollyTextMediumGray],
+                                                                                               NSParagraphStyleAttributeName: paragrapStyleCenter,
+                                                                                               NSFontAttributeName: [UIFont regularCustomFontOfSize:18]}]];
+    
+    
+    NSArray* creditsArray = @[@"FacebookSDK by Facebook", @"CKImagePicker by George Kitz", @"FacebookSDK by Facebook"];
+    for (NSString* credit in creditsArray)
+    {
+        [attributedMutString appendAttributedString: [[NSAttributedString alloc] initWithString: [NSString stringWithFormat:@"%@\n\n", credit]
+                                                                                     attributes: @{NSForegroundColorAttributeName: [UIColor dollyTextMediumGray],
+                                                                                                   NSParagraphStyleAttributeName: paragrapStyleLeft,
+                                                                                                   NSFontAttributeName: [UIFont regularCustomFontOfSize:18]}]];
+    }
+    
+    self.textView.attributedText = attributedMutString;
     
 }
 
@@ -32,10 +69,15 @@
     if(sender == self.termsButton)
     {
         
+        
+        
     }
     else if(sender == self.policyButton)
     {
         
+        
+        
     }
+    
 }
 @end
