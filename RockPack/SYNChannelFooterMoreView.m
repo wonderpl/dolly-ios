f6f621dbdd49d393acee78456a14179c3a8adb4d
Sherman Lo <sherman@rockpack.com>
2013-12-02 15:49:52 +0000

    No need to manually hide/show the activity indicator, just set hidesWhenStopped in the nib

--- RockPack/SYNChannelFooterMoreView.m
+++ RockPack/SYNChannelFooterMoreView.m
@@ -8,40 +8,34 @@
 
 #import "SYNChannelFooterMoreView.h"
 
+@interface SYNChannelFooterMoreView ()
+
+@property (nonatomic, strong) IBOutlet UIActivityIndicatorView* activityIndicator;
+
+@end
+
 @implementation SYNChannelFooterMoreView
 
-- (void) awakeFromNib
-{
-    self.activityIndicator.hidden = YES;
-    
-    self.activityIndicator.color = [UIColor colorWithRed: (11.0/255.0)
-                                                   green: (166.0/255.0)
-                                                    blue: (171.0/255.0)
-                                                   alpha: (1.0)];
-    
-    self.backgroundColor = [UIColor colorWithPatternImage: [UIImage imageNamed: @"BackgroundLoadMore"]];
+- (void)awakeFromNib {
+	[super awakeFromNib];
+	
+	self.activityIndicator.color = [UIColor colorWithRed: (11.0/255.0)
+												   green: (166.0/255.0)
+													blue: (171.0/255.0)
+												   alpha: (1.0)];
 }
 
+- (void)setShowsLoading:(BOOL)showsLoading {
+	if (_showsLoading == showsLoading)
+		return;
 
-- (void) setShowsLoading: (BOOL)showsLoading
-{
-    if (_showsLoading == showsLoading)
-        return;
-    
-    _showsLoading = showsLoading;
-    
-    if(_showsLoading)
-    {
-        self.activityIndicator.hidden = NO;
-        [self.activityIndicator startAnimating];
-        
-    }
-    else
-    {
-        [self.activityIndicator stopAnimating];
-        self.activityIndicator.hidden = YES;
-    }
+	_showsLoading = showsLoading;
+	
+	if (_showsLoading) {
+		[self.activityIndicator startAnimating];
+	} else {
+		[self.activityIndicator stopAnimating];
+	}
 }
-
 
 @end
