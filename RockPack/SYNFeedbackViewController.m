8c0579caa5ff221485619c7e6fa69df08e64d342
Michael Michailidis <mmick66@gmail.com>
2013-12-02 16:09:55 +0000

    Removing overlay for feedback

--- RockPack/SYNFeedbackViewController.m
+++ RockPack/SYNFeedbackViewController.m
@@ -10,6 +10,7 @@
 #import "SYNOAuthNetworkEngine.h"
 #import "SYNAppDelegate.h"
 #import "UIFont+SYNFont.h"
+#import "SYNMasterViewController.h"
 
 @interface SYNFeedbackViewController ()
 
@@ -17,6 +18,12 @@
 @property (nonatomic, strong) IBOutlet UILabel* titleLabel;
 @property (nonatomic, strong) IBOutlet UILabel* sliderLabel;
 @property (nonatomic, strong) IBOutlet UITextView* textView;
+
+// label
+
+@property (nonatomic, strong) IBOutlet UILabel* minValueLabel;
+@property (nonatomic, strong) IBOutlet UILabel* maxValueLabel;
+@property (nonatomic, strong) IBOutlet UILabel* currentValueLabel;
 
 @end
 
@@ -30,18 +37,31 @@
     
     self.title = NSLocalizedString(@"Feedback", nil);
     
+    ///self.currentValueLabel.hidden = YES;
+    
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Send", nil)
                                                                               style:UIBarButtonItemStyleBordered
                                                                              target:self
                                                                              action:@selector(sendButtonPressed:)];
     
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Close", nil)
-                                                                              style:UIBarButtonItemStyleBordered
-                                                                             target:self
-                                                                             action:@selector(closeButtonPressed:)];
+                                                                             style:UIBarButtonItemStyleBordered
+                                                                            target:self
+                                                                            action:@selector(closeButtonPressed:)];
+    
+    // setting fonts
     
     self.titleLabel.font = [UIFont regularCustomFontOfSize:self.titleLabel.font.pointSize];
     self.sliderLabel.font = [UIFont regularCustomFontOfSize:self.sliderLabel.font.pointSize];
+    
+    self.minValueLabel.font = [UIFont regularCustomFontOfSize:self.minValueLabel.font.pointSize];
+    self.maxValueLabel.font = [UIFont regularCustomFontOfSize:self.maxValueLabel.font.pointSize];
+    self.currentValueLabel.font = [UIFont regularCustomFontOfSize:self.currentValueLabel.font.pointSize];
+    
+    
+    // set the initial state
+    
+    [self sliderMoved:self.slider];
 }
 
 #pragma mark - Top Button Callbacks
@@ -53,7 +73,9 @@
 
 - (void) closeButtonPressed:(UIBarButtonItem*)buttonItem
 {
+    SYNAppDelegate* appDelegate = (SYNAppDelegate*)[[UIApplication sharedApplication] delegate];
     
+    [appDelegate.masterViewController removeOverlayControllerAnimated:YES];
     
 }
 
@@ -77,13 +99,29 @@
                                             }];
 }
 
+#pragma mark - UISlider
+
+- (IBAction)sliderMoved:(UISlider*)slider
+{
+    self.currentValueLabel.text = [NSString stringWithFormat:@"%0.1f", slider.value];
+    self.currentValueLabel.hidden = NO;
+    
+    
+    CGFloat xPosition = ((self.slider.frame.size.width - 34.0f) * (self.slider.value/10.0f)) + self.slider.frame.origin.x + 17.0f;
+    
+    
+    self.currentValueLabel.center = CGPointMake(xPosition, self.currentValueLabel.center.y);
+    
+    
+}
+
 #pragma mark - TextView Delegate
 
 - (void)textViewDidBeginEditing:(UITextView *)textView
 {
     if ([textView.text isEqualToString:@"placeholder text here..."]) {
         textView.text = @"";
-        textView.textColor = [UIColor blackColor]; //optional
+        textView.textColor = [UIColor blackColor]; // optional
     }
     [textView becomeFirstResponder];
 }
