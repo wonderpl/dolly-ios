64964cc80a8b9c4884d5388f318f891d1e423af9
CongLeRockPack <cong@rockpack.com>
2013-12-16 14:23:05 +0000

    UInavigation Category for transparent navigation bar

---
+++ RockPack/UINavigationController+Appearance.h
@@ -0,0 +1,17 @@
+//
+//  UINavigationController+Appearance.h
+//  dolly
+//
+//  Created by Cong on 16/12/2013.
+//  Copyright (c) 2013 Nick Banks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Appearance)
+
+-(void) setTransparent;
+-(UINavigationBar*) setDefault;
+
+
+@end
