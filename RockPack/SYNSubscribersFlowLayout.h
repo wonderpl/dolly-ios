e8f11ac781d0c264decefbf0b87cd732d6c9de16
CongLeRockPack <cong@rockpack.com>
2014-01-16 18:11:38 +0000

    Added subscriptions flow layout

---
+++ RockPack/SYNSubscribersFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  SYNSubscribersFlowLayout.h
+//  dolly
+//
+//  Created by Cong on 16/01/2014.
+//  Copyright (c) 2014 Rockpack Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNSubscribersFlowLayout : UICollectionViewFlowLayout
+
+@end
