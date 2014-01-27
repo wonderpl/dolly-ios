650f58719d2a50bdd05946a5529e5b77b3fc01a8
Sherman Lo <sherman@rockpack.com>
2014-01-27 13:28:54 +0000

    Change error message [Delivers #63733336]

--- RockPack/GKImagePicker.m
+++ RockPack/GKImagePicker.m
@@ -88,7 +88,7 @@
     if(MAX(image.size.width, image.size.height)>kMaxSuportedImageSize)
     {
         //Image too large
-        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"This image is too large", nil) message:[NSString stringWithFormat:NSLocalizedString(@"The maximum image resolution allowed for images is %dpx * %dpx.", nil), kMaxSuportedImageSize, kMaxSuportedImageSize] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"This picture is too large", nil) message:[NSString stringWithFormat:NSLocalizedString(@"The maximum size allowed for pictures is %d pixels * %d pixels. Please choose another picture.", nil), kMaxSuportedImageSize, kMaxSuportedImageSize] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
         [alert show];
         [self imagePickerControllerDidCancel:picker];
         return;
