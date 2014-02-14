c27a7b691b6b8dd05e8a7188d9062450d8299dd9
CongLeRockPack <congle@Congs-MacBook-Air.local>
2014-02-14 12:12:31 +0000

    Added Section Footer

---
+++ RockPack/SYNDiscoverySectionView.h
@@ -0,0 +1,13 @@
+//
+//  SYNDiscoverySectionView.h
+//  dolly
+//
+//  Created by Cong Le on 14/02/2014.
+//  Copyright (c) 2014 Rockpack Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYNDiscoverySectionView : UICollectionReusableView
+
+@end
---
+++ RockPack/SYNDiscoverySectionView.m
@@ -0,0 +1,31 @@
+//
+//  SYNDiscoverySectionView.m
+//  dolly
+//
+//  Created by Cong Le on 14/02/2014.
+//  Copyright (c) 2014 Rockpack Ltd. All rights reserved.
+//
+
+#import "SYNDiscoverySectionView.h"
+
+@implementation SYNDiscoverySectionView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
