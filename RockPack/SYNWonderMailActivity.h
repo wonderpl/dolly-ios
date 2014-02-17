b82f01d58c1a4ae5b468e4b8e67b566b7740433f
Sherman Lo <sherman@rockpack.com>
2014-02-17 13:54:59 +0000

    There is now only one way of sharing via email

---
+++ RockPack/SYNWonderMailActivity.h
@@ -0,0 +1,13 @@
+//
+//  SYNWonderMailActivity.h
+//  dolly
+//
+//  Created by Sherman Lo on 17/02/14.
+//  Copyright (c) 2014 Rockpack Ltd. All rights reserved.
+//
+
+#import "OWActivity.h"
+
+@interface SYNWonderMailActivity : OWActivity
+
+@end
