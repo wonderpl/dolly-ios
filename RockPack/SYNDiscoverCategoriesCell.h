da81e1abac387ff5cc35bd3699a683ba13769a17
Sherman Lo <sherman@rockpack.com>
2014-03-13 17:06:43 +0000

    Delete unused import

--- RockPack/SYNDiscoverCategoriesCell.h
+++ RockPack/SYNDiscoverCategoriesCell.h
@@ -5,7 +5,7 @@
 //  Created by Michael Michailidis on 16/10/2013.
 //  Copyright (c) 2013 Nick Banks. All rights reserved.
 //
-#import "SubGenre.h"
+
 @import UIKit;
 
 @interface SYNDiscoverCategoriesCell : UICollectionViewCell
