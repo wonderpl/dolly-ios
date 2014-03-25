19086a5421b8d257fa4be5eeaae071629b8471c9
Cong <cong@rockpack.com>
2014-03-25 16:51:28 +0000

    Profile Edit View Controller

---
+++ RockPack/SYNProfileEditViewController.h
@@ -0,0 +1,17 @@
+//
+//  SYNProfileEditViewController.h
+//  dolly
+//
+//  Created by Cong Le on 10/03/2014.
+//  Copyright (c) 2014 Wonder PL Ltd. All rights reserved.
+//
+
+#import "SYNAbstractViewController.h"
+#import "SYNProfileEditDelegate.h"
+
+@interface SYNProfileEditViewController : SYNAbstractViewController
+@property (strong, nonatomic) IBOutlet UITextView *descriptionTextView;
+@property (strong, nonatomic) NSString *descriptionString;
+@property (weak, nonatomic) id<SYNProfileEditDelegate> delegate;
+
+@end
