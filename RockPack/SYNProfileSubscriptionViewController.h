66387da2d96f021325572fa8083e4e632878887a
Cong <cong@rockpack.com>
2014-03-25 16:50:06 +0000

    Profile Subscription View controller

---
+++ RockPack/SYNProfileSubscriptionViewController.h
@@ -0,0 +1,24 @@
+//
+//  SYNProfileSubscriptionViewController.h
+//  dolly
+//
+//  Created by Cong Le on 10/03/2014.
+//  Copyright (c) 2014 Wonder PL Ltd. All rights reserved.
+//
+
+#import "SYNAbstractViewController.h"
+#import "SYNProfileSubscriptionModel.h"
+#import "SYNProfileHeader.h"
+
+@interface SYNProfileSubscriptionViewController : SYNAbstractViewController
+@property (nonatomic, strong) ChannelOwner* channelOwner;
+@property (strong, nonatomic) IBOutlet UICollectionView *cv;
+@property (nonatomic) BOOL isUserProfile;
+@property (nonatomic, strong, readonly) SYNProfileHeader* headerView;
+
+@property (nonatomic, readonly) SYNProfileSubscriptionModel *model;
+
+
+- (void) coverPhotoAnimation;
+
+@end
