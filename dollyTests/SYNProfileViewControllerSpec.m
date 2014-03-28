59c7d44c3d13f3bebe05865a2a7a447d8ac86bdc
Cong <cong@rockpack.com>
2014-03-28 17:25:22 +0000

    1 it per test

--- dollyTests/SYNProfileViewControllerSpec.m
+++ dollyTests/SYNProfileViewControllerSpec.m
@@ -9,40 +9,46 @@
 SPEC_BEGIN(SYNProfileViewControllerSpec)
 
 describe(@"SYNProfileViewController", ^{
+	SYNTrackingManager *mockManager = [SYNTrackingManager mock];
+	[SYNTrackingManager stub:@selector(sharedManager) andReturn:mockManager];
 	
+	SYNProfileViewController *viewController = [[SYNProfileViewController alloc] init];
+
 	it(@"should track screen view", ^{
-		SYNTrackingManager *mockManager = [SYNTrackingManager mock];
-		[SYNTrackingManager stub:@selector(sharedManager) andReturn:mockManager];
-		
-		SYNProfileViewController *viewController = [[SYNProfileViewController alloc] init];
 		
 //		[[mockManager should]receive:@selector(trackOwnProfileScreenView)];
 //		[[mockManager should]receive:@selector(trackOwnProfileFollowingScreenView)];
 //		[[mockManager should]receive:@selector(trackOwnProfileFollowingScreenView)];
-//		[[mockManager should]receive:@selector(trackCollectionCreatedWithName) withArguments:@"name of channel"];
 //		[[mockManager should]receive:@selector(trackAvatarPhotoUploadCompleted)];
 //		[[mockManager should]receive:@selector(trackCoverPhotoUploadCompleted)];
 //		[[mockManager should]receive:@selector(trackCreateChannelScreenView)];
 //		[[mockManager should]receive:@selector(trackUserCollectionsFollowFromScreenName) withArguments:[viewController trackingScreenName]];
 
-		
-		
 		[[mockManager should]receive:@selector(trackOtherUserProfileScreenView)];
 		[viewController viewDidAppear:YES];;
 		
-		
-		[[mockManager should]receive:@selector(trackEditProfileScreenView)];
-		[viewController editButtonTapped];
-
+	});
+	
+	
+	it(@"should track the followings tab button", ^{
 		[[mockManager should]receive:@selector(trackOtherUserCollectionFollowingScreenView)];
 		[viewController followingsTabTapped];
-		
+
+	});
+	
+	it(@"should track cover photo upload", ^{
 		[[mockManager should]receive:@selector(trackCoverPhotoUpload)];
 		[viewController updateCoverImage:nil];
-		
-		
+	});
+	
+	it(@"should track avatar photo upload", ^{
+		[[mockManager should]receive:@selector(trackAvatarUploadFromScreen:) withArguments:[viewController trackingScreenName]];
 		[viewController updateAvatarImage:nil];
-		[[mockManager should]receive:@selector(trackAvatarUploadFromScreen) withArguments:[viewController trackingScreenName]];		
+
+	});
+	
+	it(@"should track avatar photo upload", ^{
+
 		
 	});
 	
