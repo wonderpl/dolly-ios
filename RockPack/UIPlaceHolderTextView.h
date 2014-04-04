2982a7878933686bc7868131bd601ae0b5f72b95
Cong <cong@rockpack.com>
2014-04-04 17:15:02 +0100

    Placeholder with TextView

---
+++ RockPack/UIPlaceHolderTextView.h
@@ -0,0 +1,20 @@
+//
+//  UIPlaceHolderTextView.h
+//  dolly
+//
+//  Created by Cong Le on 04/04/2014.
+//  Copyright (c) 2014 Wonder PL Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPlaceHolderTextView : UITextView
+@property (nonatomic, strong) NSString *placeholder;
+@property (nonatomic, strong) UIColor *placeholderColor;
+
+- (void)textChanged:(NSNotification*)notification;
+- (void)setPlaceHolderLabelFont:(UIFont*)font;
+
+
+
+@end
