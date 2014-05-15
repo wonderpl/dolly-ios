8072977d5acd367bcfdf5f2e05b9a166c114bb56
Cong <cong@rockpack.com>
2014-05-15 10:20:09 +0100

    remove the ability to display the inboarding screen

--- RockPack/SYNInBoardingViewController.m
+++ RockPack/SYNInBoardingViewController.m
@@ -32,18 +32,18 @@
 
 
 - (void) addToViewController :(UIViewController*) vc {
-        
-        // Set frame to full screen
-        CGRect vFrame = self.view.frame;
-        vFrame.size = [[SYNDeviceManager sharedInstance] currentScreenSize];
-        self.view.frame = vFrame;
-        self.view.alpha = 0.0f;
-        
-        [vc addChildViewController:self];
-        [vc.view addSubview:self.view];
-        [UIView animateWithDuration:2.0 animations:^{
-            self.view.alpha = 1.0f;
-        }];
+//        
+//        // Set frame to full screen
+//        CGRect vFrame = self.view.frame;
+//        vFrame.size = [[SYNDeviceManager sharedInstance] currentScreenSize];
+//        self.view.frame = vFrame;
+//        self.view.alpha = 0.0f;
+//        
+//        [vc addChildViewController:self];
+//        [vc.view addSubview:self.view];
+//        [UIView animateWithDuration:2.0 animations:^{
+//            self.view.alpha = 1.0f;
+//        }];
 }
 
 - (void)screenTapped:(UITapGestureRecognizer*)tapGesture {
