010cbda41c21b9bb77870bc6f899f012ab798565
Cong <cong@rockpack.com>
2014-05-29 19:36:52 +0100

    in boarding shop motion

--- RockPack/SYNShopMotionOverlayViewController.m
+++ RockPack/SYNShopMotionOverlayViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SYNShopMotionOverlayViewController.h"
+#import "UIFont+SYNFont.h"
 
 @interface SYNShopMotionOverlayViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
@@ -16,6 +17,11 @@
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *tabButtonHorizontal;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *tabButtonHorizontalConstant;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *tabButtonVerticalConstant;
+@property (strong, nonatomic) IBOutlet UILabel *labelThisVideo;
+@property (strong, nonatomic) IBOutlet UILabel *labelWillAppear;
+@property (strong, nonatomic) IBOutlet UILabel *theMotion;
+@property (strong, nonatomic) IBOutlet UILabel *labelTapIcon;
+@property (strong, nonatomic) IBOutlet UILabel *labelTapping;
 
 @end
 
@@ -33,7 +39,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    [self.labelThisVideo setFont:[UIFont regularCustomFontOfSize:17]];
+    [self.labelWillAppear setFont:[UIFont regularCustomFontOfSize:17]];
+    [self.theMotion setFont:[UIFont regularCustomFontOfSize:17]];
+    [self.labelTapIcon setFont:[UIFont regularCustomFontOfSize:17]];
+    [self.labelTapping setFont:[UIFont regularCustomFontOfSize:17]];
 }
 
 - (void)didReceiveMemoryWarning
