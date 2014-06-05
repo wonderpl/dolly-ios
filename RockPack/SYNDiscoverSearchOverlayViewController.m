a035ddea2b2f66674bb7f0d5df26c78b781e1f2f
Cong <cong@rockpack.com>
2014-06-05 16:31:32 +0100

    New in boarding overlay text [delivers #72598130]

--- RockPack/SYNDiscoverSearchOverlayViewController.m
+++ RockPack/SYNDiscoverSearchOverlayViewController.m
@@ -12,6 +12,9 @@
 @interface SYNDiscoverSearchOverlayViewController ()
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
 @property (strong, nonatomic) IBOutlet UILabel *textLabel;
+@property (strong, nonatomic) IBOutlet UILabel *header;
+@property (strong, nonatomic) IBOutlet UILabel *foodLabel;
+@property (strong, nonatomic) IBOutlet UILabel *chefs;
 
 @end
 
@@ -22,11 +25,22 @@
 {
     [super viewDidLoad];
 	[self.titleLabel setFont:[UIFont regularCustomFontOfSize:self.titleLabel.font.pointSize]];
+	[self.chefs setFont:[UIFont semiboldCustomFontOfSize:self.chefs.font.pointSize]];
+    [self.textLabel setFont:[UIFont regularCustomFontOfSize:self.textLabel.font.pointSize]];
+
+	[self.header setFont: [UIFont semiboldCustomFontOfSize: self.header.font.pointSize]];
     
-	[self.textLabel setFont:[UIFont regularCustomFontOfSize:self.textLabel.font.pointSize]];
-    
-    
+    NSDictionary *attributes = @{
+								 NSKernAttributeName : @(2.5f),
+								 NSFontAttributeName : [UIFont lightCustomFontOfSize:self.foodLabel.font.pointSize],
+								 };
+	
+	NSAttributedString *attributedString = [[NSAttributedString alloc] initWithString:@"FOOD" attributes:attributes];
+	
+	self.foodLabel.attributedText = attributedString;
+
 }
+
 
 
 @end
