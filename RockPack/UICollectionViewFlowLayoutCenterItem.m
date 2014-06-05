22697ea1af76e44e857efa67b6cdb603e573c75f
Cong <cong@rockpack.com>
2014-06-05 15:19:08 +0100

    added UICollectionViewFlowLayoutCenterItem

---
+++ RockPack/UICollectionViewFlowLayoutCenterItem.m
@@ -0,0 +1,41 @@
+#import "UICollectionViewFlowLayoutCenterItem.h"
+
+@implementation UICollectionViewFlowLayoutCenterItem
+
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGFloat proposedContentOffsetCenterX = proposedContentOffset.x + self.collectionView.bounds.size.width * 0.5f;
+    
+    CGRect proposedRect = self.collectionView.bounds;
+    
+    // Comment out if you want the collectionview simply stop at the center of an item while scrolling freely
+    // proposedRect = CGRectMake(proposedContentOffset.x, 0.0, collectionViewSize.width, collectionViewSize.height);
+    
+    UICollectionViewLayoutAttributes* candidateAttributes;
+    for (UICollectionViewLayoutAttributes* attributes in [self layoutAttributesForElementsInRect:proposedRect])
+    {
+        
+        // == Skip comparison with non-cell items (headers and footers) == //
+        if (attributes.representedElementCategory != UICollectionElementCategoryCell)
+        {
+            continue;
+        }
+        
+        // == First time in the loop == //
+        if(!candidateAttributes)
+        {
+            candidateAttributes = attributes;
+            continue;
+        }
+        
+        if (fabsf(attributes.center.x - proposedContentOffsetCenterX) < fabsf(candidateAttributes.center.x - proposedContentOffsetCenterX))
+        {
+            candidateAttributes = attributes;
+        }
+    }
+    
+    return CGPointMake(candidateAttributes.center.x - self.collectionView.bounds.size.width * 0.5f, proposedContentOffset.y);
+    
+}
+
+@end
