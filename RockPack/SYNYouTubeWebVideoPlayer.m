2a0d7418f9535170e7d63ada71c734429fc2bb6f
Cong <cong@rockpack.com>
2014-06-10 08:05:51 +0100

    webview to weak

--- RockPack/SYNYouTubeWebVideoPlayer.m
+++ RockPack/SYNYouTubeWebVideoPlayer.m
@@ -23,7 +23,7 @@ typedef NS_ENUM(NSInteger, SYNYouTubeVideoPlayerState) {
 
 @interface SYNYouTubeWebVideoPlayer () <UIWebViewDelegate>
 
-@property (nonatomic, strong) UIWebView *youTubeWebView;
+@property (nonatomic, weak) UIWebView *youTubeWebView;
 
 @property (nonatomic, assign) SYNYouTubeVideoPlayerState youTubePlayerState;
 
