bdacb20b705cdef20d9ff1e00f662a5419f1e301
Cong <cong@rockpack.com>
2014-07-03 12:26:34 +0100

    removed boiler plater code

--- RockPack/SYNShopMotionOverlayViewController.m
+++ RockPack/SYNShopMotionOverlayViewController.m
@@ -27,15 +27,6 @@
 
 @implementation SYNShopMotionOverlayViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -47,12 +38,6 @@
     [self.theMotion setFont:[UIFont regularCustomFontOfSize:self.theMotion.font.pointSize]];
     [self.labelTapIcon setFont:[UIFont regularCustomFontOfSize:self.labelTapIcon.font.pointSize]];
     [self.labelTapping setFont:[UIFont regularCustomFontOfSize:self.labelTapping.font.pointSize]];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)viewWillLayoutSubviews
