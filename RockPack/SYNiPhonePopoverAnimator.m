2da648bed9b07aa97bf06f4025150617407bb857
Cong <cong@rockpack.com>
2014-09-10 12:09:08 +0100

    Modal video controller push from bottom for IPad

--- RockPack/SYNiPhonePopoverAnimator.m
+++ RockPack/SYNiPhonePopoverAnimator.m
@@ -26,20 +26,79 @@
 		
 		CGRect startFrame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
 									   CGRectGetHeight(containerView.frame),
-									   CGRectGetWidth(toViewController.view.frame),
+									   CGRectGetWidth(fromViewController.view.frame),
 									   CGRectGetHeight(toViewController.view.frame));
 		toViewController.view.frame = startFrame;
 		
+        if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortrait) {
+            CGRect startFrame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                           CGRectGetHeight(containerView.frame),
+                                           CGRectGetWidth(fromViewController.view.frame),
+                                           CGRectGetHeight(toViewController.view.frame));
+            toViewController.view.frame = startFrame;
+        }
+        
+        if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeLeft) {
+            CGRect startFrame = CGRectMake(-CGRectGetHeight(fromViewController.view.frame)-CGRectGetHeight(fromViewController.view.frame),
+                                           0,
+                                           CGRectGetWidth(fromViewController.view.frame),
+                                           CGRectGetHeight(fromViewController.view.frame));
+            toViewController.view.frame = startFrame;
+        }
+        
+        if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeRight) {
+
+            CGRect startFrame = CGRectMake(CGRectGetHeight(fromViewController.view.frame)+CGRectGetHeight(fromViewController.view.frame),
+                                           0,
+                                           CGRectGetWidth(fromViewController.view.frame),
+                                           CGRectGetHeight(fromViewController.view.frame));
+            toViewController.view.frame = startFrame;
+        }
+        
+        if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortraitUpsideDown) {
+            CGRect startFrame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                           -CGRectGetHeight(toViewController.view.frame),
+                                           CGRectGetWidth(fromViewController.view.frame),
+                                           CGRectGetHeight(toViewController.view.frame));
+            toViewController.view.frame = startFrame;
+        }
+        
 		[containerView addSubview:tapDismissView];
 		[containerView addSubview:toViewController.view];
 		
 		[UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
 			tapDismissView.alpha = 1.0;
-			
-			toViewController.view.frame = CGRectMake(CGRectGetMinX(startFrame),
-													 CGRectGetMinY(startFrame) - CGRectGetHeight(startFrame),
-													 CGRectGetWidth(startFrame),
-													 CGRectGetHeight(startFrame));
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortrait) {
+                toViewController.view.frame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                                         CGRectGetHeight(containerView.frame) - CGRectGetHeight(toViewController.view.frame),
+                                                         CGRectGetWidth(fromViewController.view.frame),
+                                                         CGRectGetHeight(toViewController.view.frame));
+            }
+
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeLeft) {
+                toViewController.view.frame = CGRectMake(0,
+                                                         0,
+                                                         CGRectGetHeight(startFrame),
+                                                         CGRectGetHeight(fromViewController.view.frame));
+
+            }
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeRight) {
+                
+                toViewController.view.frame = CGRectMake(CGRectGetWidth(fromViewController.view.frame) - CGRectGetHeight(startFrame),
+                                                         0,
+                                                         CGRectGetHeight(startFrame),
+                                                         CGRectGetHeight(fromViewController.view.frame));
+            }
+
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortraitUpsideDown) {
+                toViewController.view.frame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                                         0,
+                                                         CGRectGetWidth(fromViewController.view.frame),
+                                                         CGRectGetHeight(toViewController.view.frame));
+            }
+            
 		} completion:^(BOOL finished) {
 			[transitionContext completeTransition:YES];
 		}];
@@ -51,10 +110,40 @@
 		
 		[UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
 			tapDismissView.alpha = 0.0;
-			fromViewController.view.frame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
-													 CGRectGetMaxY(containerView.frame),
-													 CGRectGetWidth(toViewController.view.frame),
-													 CGRectGetHeight(toViewController.view.frame));
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortrait) {
+                CGRect endFrame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                               CGRectGetHeight(containerView.frame),
+                                               CGRectGetWidth(fromViewController.view.frame),
+                                               CGRectGetHeight(toViewController.view.frame));
+                fromViewController.view.frame = endFrame;
+            }
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeLeft) {
+                CGRect endFrame = CGRectMake(-CGRectGetHeight(fromViewController.view.frame)-CGRectGetHeight(fromViewController.view.frame),
+                                               0,
+                                               CGRectGetWidth(fromViewController.view.frame),
+                                               CGRectGetHeight(fromViewController.view.frame));
+                fromViewController.view.frame = endFrame;
+            }
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationLandscapeRight) {
+                
+                CGRect endFrame = CGRectMake(CGRectGetHeight(fromViewController.view.frame)+CGRectGetHeight(fromViewController.view.frame),
+                                               0,
+                                               CGRectGetWidth(fromViewController.view.frame),
+                                               CGRectGetHeight(fromViewController.view.frame));
+                fromViewController.view.frame = endFrame;
+            }
+            
+            if ([[UIApplication sharedApplication] statusBarOrientation] == UIDeviceOrientationPortraitUpsideDown) {
+                CGRect endFrame = CGRectMake(CGRectGetMinX(toViewController.view.frame),
+                                               -CGRectGetHeight(toViewController.view.frame),
+                                               CGRectGetWidth(fromViewController.view.frame),
+                                               CGRectGetHeight(toViewController.view.frame));
+                fromViewController.view.frame = endFrame;
+            }
+
 		} completion:^(BOOL finished) {
 			[tapDismissView removeFromSuperview];
 			[fromViewController.view removeFromSuperview];
