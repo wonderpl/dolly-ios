c47b07bf796344682e5d057b2759365454cb58bf
Cong <cong@rockpack.com>
2014-10-01 14:12:33 +0100

    Tracking in widget [delivers #79853316]

--- wonder-today-extension/TodayViewController.m
+++ wonder-today-extension/TodayViewController.m
@@ -10,18 +10,22 @@
 #import <NotificationCenter/NotificationCenter.h>
 #import "VideoInstance.h"
 #import "AbstractCommon.h"
-#import "UIFont+SYNFont.h"
+#import <GAI.h>
+#import <GAIFields.h>
+#import <GAIDictionaryBuilder.h>
+#import "SYNTrackingManager.h"
+#import <Reachability.h>
+@import CoreTelephony;
 
 #define IS_IPHONE ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone )
 #define IS_IPAD ( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad )
+static const NSInteger TrackingDimensionConnection = 6;
 
 @interface TodayViewController () <NCWidgetProviding>
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
 @property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
-@property (strong, nonatomic) IBOutlet UIButton *clickToMoreButton;
 @property (strong, nonatomic) IBOutlet UIButton *videoButton;
 @property (strong, nonatomic) NSDictionary *videoDictionary;
-@property (strong, nonatomic) IBOutlet UILabel *addedByLabel;
 @property (strong, nonatomic) IBOutlet UIButton *curatedByButton;
 @property (strong, nonatomic) IBOutlet UILabel *watchLabel;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *videoWidth;
@@ -30,6 +34,8 @@
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *labelRight;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *topPlayImage;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *leftPlayImage;
+@property (strong, nonatomic) CTTelephonyNetworkInfo *networkInfo;
+@property (nonatomic, strong) Reachability *reachability;
 
 @end
 
@@ -48,9 +54,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUpVideo];
-    
+
+    self.networkInfo = [[CTTelephonyNetworkInfo alloc] init];
+    NSString *hostname = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"APIHostName"];
+    self.reachability = [Reachability reachabilityWithHostname:hostname];
+    [self.reachability startNotifier];
+
     if ([[UIScreen mainScreen] bounds].size.width > 600) {
-        
         [self.labelRight setConstant:223];
         [self.labelTop setConstant:12];
         [self.topPlayImage setConstant:97];
@@ -58,11 +68,13 @@
         [self.videoWidth setConstant:320];
         [self.videoHeight setConstant:190];
     }
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self setUpVideo];
+    [self widgetTracking];
 }
 
 - (void)setUpVideo{
@@ -71,7 +83,6 @@
     self.videoDictionary = [[mySharedDefaults dictionaryForKey:@"videoData"] mutableCopy];
     
     if ([mySharedDefaults boolForKey:@"isVideo"]) {
-        [self setUpClickToMore];
         [self setUpVideoDescription];
         
         [self.titleLabel setText:self.videoDictionary[@"title"]];
@@ -82,13 +93,11 @@
         
         [self.videoButton setImage:myImage forState:UIControlStateNormal];
         
-        [self.addedByLabel setText:self.videoDictionary[@"label"]];
         [self.curatedByButton setTitle:self.videoDictionary[@"channelOwnerName"] forState:UIControlStateNormal];
         [self.watchLabel setText:self.videoDictionary[@"duration"]];
         
         self.titleLabel.hidden = NO;
         self.videoButton.hidden = NO;
-        self.addedByLabel.hidden = NO;
         self.curatedByButton.hidden = NO;
         self.watchLabel.hidden = NO;
     }
@@ -97,8 +106,6 @@
 - (void)setUpViews :(BOOL) isVideo {
     self.titleLabel.hidden = isVideo;
     self.videoButton.hidden = isVideo;
-    self.clickToMoreButton.hidden = isVideo;
-    self.addedByLabel.hidden = isVideo;
     self.curatedByButton.hidden = isVideo;
     self.watchLabel.hidden = isVideo;
 }
@@ -127,24 +134,6 @@
     }
 }
 
-- (void)setUpClickToMore {
-    self.clickToMoreButton.layer.cornerRadius = 5.0;
-    NSString *linkTitle = self.videoDictionary[@"linkTitle"];
-    self.clickToMoreButton.hidden = ([linkTitle length] == 0);
-    [self.clickToMoreButton setTitle:linkTitle forState:UIControlStateNormal];
-}
-
-- (IBAction)avatarButtonTapped:(id)sender {
-    NSUserDefaults *mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.wonderapps"];
-    
-    NSString *channelOwnerId = [mySharedDefaults objectForKey:@"channelOwnerId"];
-    
-    NSString *urlString = [NSString stringWithFormat:@"wonderpldev://-/%@/",channelOwnerId];
-    NSURL *url = [NSURL URLWithString:urlString];
-    [self.extensionContext openURL:url completionHandler:nil];
-
-}
-
 - (void)userDefaultsDidChange:(NSNotification *)notification {
     [self setUpVideo];
 }
@@ -152,20 +141,6 @@
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
     [self setUpVideo];
     completionHandler(NCUpdateResultNewData);
-}
-
-- (IBAction)clickToMoreButton:(id)sender {
-    
-    NSUserDefaults *mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.wonderapps"];
-    
-    
-        NSString *channelId = [mySharedDefaults objectForKey:@"channelId"];
-        NSString *videoId = [mySharedDefaults objectForKey:@"videoId"];
-        
-        NSString *urlString = [NSString stringWithFormat:@"wonderpl://-/channels/%@/videos/%@/-/", channelId,videoId];
-        NSURL *url = [NSURL URLWithString:urlString];
-        [self.extensionContext openURL:url completionHandler:nil];
-
 }
 
 - (IBAction)openApp:(id)sender {
@@ -180,14 +155,6 @@
         NSURL *url = [NSURL URLWithString:urlString];
         [self.extensionContext openURL:url completionHandler:nil];
     }
-
-}
-
-- (UIColor*)dollyGreen {
-        return [UIColor colorWithRed: 74.0f / 255.0f
-                               green: 175.0f / 255.0f
-                                blue: 92.0f / 255.0f
-                               alpha: 1.0f];
 }
 
 -(NSString *) stringByStrippingHTMLFromString:(NSString*)s {
@@ -195,6 +162,55 @@
     while ((r = [s rangeOfString:@"<[^>]+>" options:NSRegularExpressionSearch]).location != NSNotFound)
         s = [s stringByReplacingCharactersInRange:r withString:@""];
     return s;
+}
+
+#pragma mark - Tracking
+
+- (void) widgetTracking {
+    [self setUpGATracking];
+    
+    [[self defaultTracker] set:kGAIScreenName value:@"Widget"];
+    [[self defaultTracker] send:[[GAIDictionaryBuilder createAppView] build]];
+}
+
+- (void) setUpGATracking {
+    GAI *gai = [GAI sharedInstance];
+    [gai trackerWithTrackingId:kGoogleAnalyticsId];
+    gai.trackUncaughtExceptions = YES;
+    gai.dispatchInterval = 30;
+    [self setConnectionDimension:[self currentConnectionString]];
+}
+
+- (id<GAITracker>)defaultTracker {
+    return [[GAI sharedInstance] defaultTracker];
+}
+
+- (void)setConnectionDimension:(NSString *)connection {
+    [[self defaultTracker] set:[GAIFields customDimensionForIndex:TrackingDimensionConnection] value:connection];
+}
+
+- (NSString *)currentConnectionString {
+    if ([self.reachability isReachableViaWiFi]) {
+        return @"wifi";
+    }
+    if ([self.reachability isReachableViaWWAN]) {
+        return [self cellTechnologyGeneration:self.networkInfo.currentRadioAccessTechnology];
+    }
+    return @"none";
+}
+
+- (NSString *)cellTechnologyGeneration:(NSString *)technology {
+    return (@{ CTRadioAccessTechnologyGPRS         : @"2g",
+               CTRadioAccessTechnologyEdge         : @"2g",
+               CTRadioAccessTechnologyWCDMA        : @"3g",
+               CTRadioAccessTechnologyHSDPA        : @"3g",
+               CTRadioAccessTechnologyHSUPA        : @"3g",
+               CTRadioAccessTechnologyCDMA1x       : @"3g",
+               CTRadioAccessTechnologyCDMAEVDORev0 : @"3g",
+               CTRadioAccessTechnologyCDMAEVDORevA : @"3g",
+               CTRadioAccessTechnologyCDMAEVDORevB : @"3g",
+               CTRadioAccessTechnologyeHRPD        : @"3g",
+               CTRadioAccessTechnologyLTE          : @"4g" }[technology] ?: @"unknown");
 }
 
 @end
